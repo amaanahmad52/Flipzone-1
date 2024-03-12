0f89b6ed3c36bccb57fd47a47bcaa2895bf1c4d2
StuartYoungA <aa21chb0a03@student.nitw.ac.in>
2024-03-13 00:06:39 +0530

    solver script error in stripe

--- backend/Controllers/PaymentController.js
+++ backend/Controllers/PaymentController.js
@@ -5,40 +5,145 @@ const stripe = require('stripe')('sk_test_51Oad13SET0Tbrnai1SP8oHBIRJ37u406NcTo0
 
 
 
-exports.processPayment =asynchandler(async(req,res,next)=>{
-    const myPayment=await stripe.paymentIntents.create({
-        description: 'Flipzone services',
-        amount: req.body.amount,
-        currency: 'inr',
-        metadata:{
-            company:"Flipzone"
-        },
-        shipping: {
-            name: 'Flipzones',
-            address: {
-              line1: 'GandhiNagar',
-              postal_code: '22005',
-              city: 'Kanpur',
-              state: 'UP',
-              country: 'IN',
+exports.processPayment =async(req,res,next)=>{
+    try {
+        const myPayment=await stripe.paymentIntents.create({
+            description: 'Flipzone services',
+            amount: req.body.amount,
+            currency: 'inr',
+            metadata:{
+                company:"Flipzone"
             },
-          },
-        automatic_payment_methods: {
-                enabled: true,
+            shipping: {
+                name: 'Flipzones',
+                address: {
+                  line1: 'GandhiNagar',
+                  postal_code: '22005',
+                  city: 'Kanpur',
+                  state: 'UP',
+                  country: 'IN',
+                },
+              },
+            automatic_payment_methods: {
+                    enabled: true,
+            }
+           
+        });
+        res.status(200).json({
+                succes:true,
+                client_secret: myPayment.client_secret
+        })
+    }catch (e) {
+        switch (e.type) {
+          case 'StripeCardError':
+            console.log(`A payment error occurred: ${e.message}`);
+            break;
+          case 'StripeInvalidRequestError':
+            console.log('An invalid request occurred.');
+            break;
+          default:
+            console.log('Another problem occurred, maybe unrelated to Stripe.');
+            break;
         }
-       
-    });
-    res.status(200).json({
-            succes:true,
-            client_secret: myPayment.client_secret
-    })
-})
+      }
+}
 
 
 //react will get publishable key by this response , so that it can use it for generating token
 
 exports.getPublishableKey = asynchandler(async(req, res, next)=>{
     const key=process.env.STRIPE_API_KEY
-  
+    
     res.status(200).json({key:key})
 })
+
+
+
+
+
+
+
+
+
+
+
+// const dotenv=require("dotenv")
+// dotenv.config()
+// const asynchandler=require("../utils/asynchandler")
+// const stripe = require('stripe')('sk_test_51Oad13SET0Tbrnai1SP8oHBIRJ37u406NcTo09WGFODoj2X9KRmOhMbbMbIrBZwQP3nIZ03daMc22Pse612Iec6B00pZOsxrFR');
+
+
+
+// exports.processPayment =asynchandler(async(req,res,next)=>{
+//     const myPayment=await stripe.paymentIntents.create({
+//         description: 'Flipzone services',
+//         amount: req.body.amount,
+//         currency: 'inr',
+//         metadata:{
+//             company:"Flipzone"
+//         },
+//         shipping: {
+//             name: 'Flipzones',
+//             address: {
+//               line1: 'GandhiNagar',
+//               postal_code: '22005',
+//               city: 'Kanpur',
+//               state: 'UP',
+//               country: 'IN',
+//             },
+//           },
+//         automatic_payment_methods: {
+//                 enabled: true,
+//         }
+       
+//     });
+//     res.status(200).json({
+//             succes:true,
+//             client_secret: myPayment.client_secret
+//     })
+// })
+
+
+// //react will get publishable key by this response , so that it can use it for generating token
+
+// exports.getPublishableKey = asynchandler(async(req, res, next)=>{
+//     const key=process.env.STRIPE_API_KEY
+  
+//     res.status(200).json({key:key})
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
